0d3d38039835d5a9210baf1bdb72b9ec822d0cca
Sevastian Kozhunov <sky.seva@gmail.com>
2020-09-21 19:52:38 +0200

    Create SampleScript.cs

---
+++ Assets/SampleScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour {
+
+    private void Start() {
+         
+    }
+
+    private void Update() {
+        
+    }
+
+}
